f864c68110481c5ed8499e28d0165e4a9a49b550
prpereiras89 <prpereiras89@gmail.com>
2020-02-14 12:56:10 -0300

    d and e

--- google_hashcode.py
+++ google_hashcode.py
@@ -1,6 +1,3 @@
-import sys
-sys.setrecursionlimit(10000)
-
 total_slices = 0
 maximum = 0
 type_pizza = []
@@ -17,11 +14,15 @@ def combinationUtil(array, data, start, end, index, p):
 	if index == p:
 		s = 0
 		aux_array = []
+		indexpos = 0
 		for j in range(0,p):
 			s += data[j]
-			aux_array.append(array.index(data[j]))
+			indexpos = array.index(data[j], indexpos)
+			aux_array.append(array.index(data[j], indexpos))
+			indexpos += 1
 
-		if total_slices <= s and s <= maximum:
+
+		if total_slices < s and s <= maximum:
 			total_slices = s
 			type_pizza = aux_array
 		return
@@ -36,32 +37,46 @@ def combinationUtil(array, data, start, end, index, p):
 		i += 1
 
 
+def interative(array):
+	global total_slices
+	global maximum
+	global type_pizza
+	result = 0
+
+	for i in reversed(range(len(array))):
+		aux = array[i]
+		if (aux + result) <= maximum:
+			result += aux
+			total_slices = result
+			type_pizza.append(i)
 
 
-
-f = open("d_quite_big.in", "r")
+f = open("e_also_big.in", "r")
 first_line = f.readline().split()
-
 maximum = int(first_line[0])
 pizzas_slices_amount = f.readline().split()
 f.close()
-
 pizzas_slices_amount = [int(i) for i in pizzas_slices_amount]
 
-aux = len(pizzas_slices_amount)+1
-while aux > 0:
-	printCombination(pizzas_slices_amount, aux)
-	aux -= 1
-	if total_slices == maximum:
-			break
+if(len(pizzas_slices_amount) < 50):
+	aux = len(pizzas_slices_amount)+1
+	while aux > 0:
+		printCombination(pizzas_slices_amount, aux)
+		aux -= 1
+		if total_slices == maximum:
+				break
+else:
+	interative(pizzas_slices_amount)
+	type_pizza.sort()
+
 
 print("total slices: " + str(total_slices))
 print("types of pizza: " + str(len(type_pizza)))
 print("pizzas: " + str(type_pizza))
+print("\n\n")
 
-f = open("d_quite_big.out","w+")
+f = open("e_also_big.out","w+")
 f.write(str(len(type_pizza)) + "\n")
-
 for i in range(0, len(type_pizza)):
 	f.write(str(type_pizza[i]) + " ")
 f.close()
